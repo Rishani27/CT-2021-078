5c7e9a4c523e5aebe776a30b7a610c63c6e4f2de
Rishani27 <rishanipradeepa05@gmail.com>
2025-03-02 16:44:44 +0530

    Add files via upload

---
+++ CT_2021_078/JAVA/src/Q_01/Sheetqu_01.java
@@ -0,0 +1,14 @@
+package Q_01;
+import javax.swing.*;
+
+
+public class Sheetqu_01 {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(800,600);
+        myWindow.setTitle("Welcome to Java");
+        myWindow.setVisible(true);
+
+    }
+}
